a893d1e7298a3fd49fc3f6146ecf629c51f02381
m3 <mlamont@gmail.com>
2025-10-02 15:36:45 -0400

    installed a bunch

---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+
+module.exports = {
+  solidity: "0.8.28",
+};
